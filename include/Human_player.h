797d30e2e4d7525264ed9738bf965044397d14d6
Justin Ho <justinhodev@gmail.com>
2017-04-06 20:35:05 -0700

    Update Human_player.h

--- include/Human_player.h
+++ include/Human_player.h
@@ -3,18 +3,37 @@
 #ifndef HUMAN_PLAYER_H_
 #define HUMAN_PLAYER_H_
 
-#include "player.h"
+#include <vector>
+
+#include "Player.h"
+#include "Board.h"
+#include "Tile.h"
 
 namespace reversi {
 
-class Human_player extends Player {
+class Human_player : public Player {
 	private:
+		int pieces;
+		int score;
+		Tile color;
+
+		std::vector<Board::direction> avail_move;
 
 	public:
-		Human_player();
+		Human_player(const Tile& t);
 
-		~Human_player();
-}
+		~Human_player() { }
+
+		void invalid_move();
+
+		void move(Board& b, int row, int col, Computer_player cp);
+
+		virtual int current_score() const;
+
+		virtual int num_pieces() const;
+
+		virtual Tile color() const;
+};
 }
 
 #endif //HUMAN_PLAYER_H_
