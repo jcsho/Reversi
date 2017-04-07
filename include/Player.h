95cc8d5ed4f6b17c4e91577ea9a78786eb53780a
Justin Ho <justinhodev@gmail.com>
2017-04-06 20:33:23 -0700

    Update Player.h

--- include/Player.h
+++ include/Player.h
@@ -3,15 +3,21 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
+#include "Board.h"
+#include "Tile.h"
+
 namespace reversi {
 
 class Player {
-	private:
-
 	public:
-		Player();
 
-		~Player();
+		virtual ~Player() { }
+
+		virtual int current_score() const = 0;
+
+		virtual int num_pieces() const = 0;
+		
+		virtual Tile color() const = 0;
 }
 }
 
